424a54c26f132cda41cee6073828750fc718115b
kristjanjansen <kristjan.jansen@gmail.com>
2021-06-02 19:44:52 +0300

    chore: cleanup

--- cypress/plugins/index.ts
+++ cypress/plugins/index.ts
@@ -2,7 +2,7 @@ import { startDevServer } from "@cypress/vite-dev-server";
 import { addMatchImageSnapshotPlugin } from "cypress-image-snapshot/plugin";
 import vue from "@vitejs/plugin-vue";
 
-module.exports = (on, config) => {
+export default (on, config) => {
   on("dev-server:start", async (options) =>
     startDevServer({
       options,
